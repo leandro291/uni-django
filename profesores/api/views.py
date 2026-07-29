e9c61c682cf278e41a7daae922a78a6a73a6129b
leandro291 <lenadrito1@gmail.com>
2026-07-29 10:02:47 -0500

    Añadir vista para gestionar asignaciones en la API de profesores

--- profesores/api/views.py
+++ profesores/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-from profesores.models import Profesor, Curso
-from profesores.api.serializers import ProfesorSerializer, CursoSerializer
+from rest_framework import generics
+from profesores.models import Profesor, Curso, Asignaciones
+from profesores.api.serializers import ProfesorSerializer, CursoSerializer, AsignacionesSerializer
 
 class ProfesorViewSet(ModelViewSet):
     queryset = Profesor.objects.all()
@@ -9,4 +10,10 @@ class ProfesorViewSet(ModelViewSet):
 class CursoViewSet(ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class AsignacionesViewSet(generics.ListCreateAPIView):
+    serializer_class = AsignacionesSerializer
+
+    def get_queryset(self):
+        return Asignaciones.objects.filter(profesor_id=self.kwargs['profesor_pk'])
 
