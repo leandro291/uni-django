6569f16a6530d9823dd2de7a84e3196cec5058a7
leandro291 <lenadrito1@gmail.com>
2026-07-30 09:50:52 -0500

    Actualizar vistas en las rutas de la API de alumnos para utilizar las listas de matriculas

--- alumnos/api/urls.py
+++ alumnos/api/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from alumnos.api.views import AlumnoViewSet, MatriculaAlumnoViewSet, MatriculaAsignacionViewSet
+from alumnos.api.views import AlumnoViewSet, MatriculasAlumnoList, MatriculasAsignacionListCreate
 
 router_alumnos = DefaultRouter()
 router_alumnos.register('alumnos', AlumnoViewSet, basename='alumno')
 
 urlpatterns = [
     path('', include(router_alumnos.urls)),
-    path('alumnos/<int:alumno_pk>/matriculas/', MatriculaAlumnoViewSet.as_view(), name='matriculas-alumno'),
-    path('asignaciones/<int:asignacion_pk>/matriculas/', MatriculaAsignacionViewSet.as_view(), name='matriculas-asignacion'),
+    path('alumnos/<int:alumno_pk>/matriculas/', MatriculasAlumnoList.as_view(), name='matriculas-alumno'),
+    path('asignaciones/<int:asignacion_pk>/matriculas/', MatriculasAsignacionListCreate.as_view(), name='matriculas-asignacion'),
 ]
