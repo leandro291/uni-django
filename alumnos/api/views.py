27e5bbe7f2e4c56f3e9426090f6cd6bd3fc14e95
leandro291 <lenadrito1@gmail.com>
2026-07-30 09:51:06 -0500

    Renombrar clases de vistas de matrícula para mayor claridad y optimizar consultas con select_related

--- alumnos/api/views.py
+++ alumnos/api/views.py
@@ -7,14 +7,14 @@ class AlumnoViewSet(ModelViewSet):
     queryset = Alumno.objects.all()
     serializer_class = AlumnoSerializer
 
-class MatriculaAlumnoViewSet(generics.ListAPIView):
+class MatriculasAlumnoList(generics.ListAPIView):
     serializer_class = MatriculaSerializer
 
     def get_queryset(self):
-        return Matricula.objects.filter(alumno_id=self.kwargs['alumno_pk'])
+        return Matricula.objects.filter(alumno_id=self.kwargs['alumno_pk']).select_related('alumno', 'asignacion__curso')
 
-class MatriculaAsignacionViewSet(generics.ListCreateAPIView):
+class MatriculasAsignacionListCreate(generics.ListCreateAPIView):
     serializer_class = MatriculaSerializer
 
     def get_queryset(self):
-        return Matricula.objects.filter(asignacion_id=self.kwargs['asignacion_pk'])
+        return Matricula.objects.filter(asignacion_id=self.kwargs['asignacion_pk']).select_related('alumno', 'asignacion__curso')
