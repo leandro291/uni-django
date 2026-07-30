bdf493cb7f7f8b31740b8cf59eaa563976c9e779
leandro291 <lenadrito1@gmail.com>
2026-07-29 20:00:54 -0500

    Añadir vista de asignaciones para cursos en la API de profesores

--- profesores/api/urls.py
+++ profesores/api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from profesores.api.views import ProfesorViewSet, CursoViewSet, AsignacionesViewSet
+from profesores.api.views import ProfesorViewSet, CursoViewSet, AsignacionesViewSet, AsignacionesCursoViewSet
 from django.urls import path, include
 
 router_profesores = DefaultRouter()
@@ -10,4 +10,5 @@ router_profesores.register('cursos', CursoViewSet, basename='curso')
 urlpatterns = [
     path('', include(router_profesores.urls)),
     path('profesores/<int:profesor_pk>/asignaciones/', AsignacionesViewSet.as_view(), name='asignaciones'),
+    path('cursos/<int:curso_pk>/asignaciones/', AsignacionesCursoViewSet.as_view(), name='asignaciones-curso'),
 ]
