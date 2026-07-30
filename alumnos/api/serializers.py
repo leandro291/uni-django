bef86946c05c439ffbd8f59f457fb7e9dbdd8068
leandro291 <lenadrito1@gmail.com>
2026-07-30 09:50:40 -0500

    Especificar campos en los serializadores de Alumno y Matricula para mayor control y claridad

--- alumnos/api/serializers.py
+++ alumnos/api/serializers.py
@@ -4,10 +4,10 @@ from alumnos.models import Alumno, Matricula
 class AlumnoSerializer(ModelSerializer):
     class Meta:
         model = Alumno
-        fields = '__all__'
+        fields = ['id', 'nombre', 'codigo_estudiante', 'email']
 
 class MatriculaSerializer(ModelSerializer):
     class Meta:
         model = Matricula
-        fields = '__all__'
-        
+        fields = ['id', 'alumno', 'asignacion', 'fecha_matricula', 'estado', 'nota_final']
+        read_only_fields = ['fecha_matricula']
