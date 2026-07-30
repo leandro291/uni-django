3b05a1026743cd5653b90092925545b28774daa6
leandro291 <lenadrito1@gmail.com>
2026-07-29 20:44:06 -0500

    Añadir inclusión de rutas para la API de alumnos

--- tablas_intermedias/urls.py
+++ tablas_intermedias/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/', include('profesores.api.urls')),
+    path('api/', include('alumnos.api.urls')),
 ]
